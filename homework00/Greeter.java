08758293a9a5ede7cc26c87df47f78e36975c329
Rohan Bramhe <rbramhe@lion.lmu.edu>
2018-01-17 08:47:08 -0800

    added file header

---
+++ homework00/Greeter.java
@@ -0,0 +1,25 @@
+/** ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ *  File name     :  Greeter.java
+ *  Purpose       :  Says "Hello world"; first program; provides a header template for Java programs
+ *  Author        :  Rohan Bramhe
+ *  Date          :  2018-01-16
+ *  Description   :  Outputs "Hello world!"
+ *  Notes         :  None
+ *  Warnings      :  None
+ *  Exceptions    :  None
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ *  Revision History
+ *  ---------------
+ *            Rev      Date     Modified by:  Reason for change/modification
+ *           -----  ----------  ------------  -----------------------------------------------------------
+ *  @version 1.0.0  2017-01-02  Rohan Bramhe  Initial writing and release
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+Public class Greeter {
+
+Public static void main (String args[] ) {
+
+System.out.println ( “Hello world!” );
+
+}
+
+}
--- homework00/Hello.java
+++
@@ -1,9 +0,0 @@
-Public class Hello {
-
-Public static void main (String args[] ) {
-
-System.out.println ( “Hello world!” );
-
-}
-
-}
